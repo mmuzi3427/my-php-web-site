20c30c6f7c6cb934333adbeed21576e35c419f18
PyDev <pydev4150@gmail.com>
2026-06-06 08:07:01 +0500

    Create main.js

---
+++ site-v1/assets/main.js
@@ -0,0 +1,7 @@
+const tg = window.Telegram.WebApp;
+//tg.ready();
+//tg.requestFullscreen();
+//tg.expand();
+//https://telegram.org/js/telegram-web-app.js?62
+tg.showAlert("Salom xush kelibsiz!");
+//tg.showConfirm("...")
