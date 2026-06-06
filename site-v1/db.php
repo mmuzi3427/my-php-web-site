00336ecf4d103ff8bb77ac31e6b6b0863beb568b
PyDev <pydev4150@gmail.com>
2026-06-06 09:41:55 +0500

    Refactor database connection to use environment variables

--- site-v1/db.php
+++ site-v1/db.php
@@ -1,14 +1,23 @@
 <?php
-$servername = "dpg-d8hpu8uq1p3s73dv0g8g-a";
-$username = "mydb_j6ea_user";
-$password = "pwen7B20OZAzaSuplkCdeBSOzGKjiij0";
-$dbname = "mydb_j6ea";
+// Bular xavfsiz va hammaga ko'rinishi mumkin bo'lgan ma'lumotlar
+$db   = 'mydb_j6ea';                  
+$user = 'mydb_j6ea_user';             
+$port = '5432';                       
+
+// MAXFIY MA'LUMOTLAR - Render'dagi Environment bo'limidan o'qib olinadi:
+$host = getenv('DB_HOST');
+$pass = getenv('DB_PASSWORD'); 
 
 try {
-  $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db;";
+    
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    
+} catch (PDOException $e) {
+    // Xavfsizlik yuzasidan xatolik ichidagi haqiqiy parollarni ekranga chiqarmaslik uchun:
+    die("Baza bilan aloqa o'rnatilmadi.");
 }
 ?>
