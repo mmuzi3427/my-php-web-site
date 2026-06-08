97cacbfc91031a2aa2bf35604ec659e446363d81
PyDev <pydev4150@gmail.com>
2026-06-08 12:41:35 +0500

    Create db.php

---
+++ db/db.php
@@ -0,0 +1,22 @@
+<?php
+$host = getenv('DB_HOST');
+$port = 23488;
+$db   = 'defaultdb';
+$user = 'avnadmin';
+$pass = getenv('DB_PASSWORD'); // "CLICK_TO_REVEAL_PASSWORD" ustiga bosib olasiz
+
+// Aiven xavfsizlik uchun SSL talab qiladi (SSL mode: REQUIRED)
+$options = [
+    PDO::MYSQL_ATTR_SSL_CA => true, 
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    
+    echo "Ulanish muvaffaqiyatli bajarildi! Baza toʻgʻri ishlayapti.";
+} catch (\PDOException $e) {
+    echo "Ulanishda xatolik: " . $e->getMessage();
+}
