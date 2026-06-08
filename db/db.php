3f1faa736b4377adcb7a6770d65dfd7f33bd546b
PyDev <pydev4150@gmail.com>
2026-06-08 13:06:28 +0500

    Enable SSL for database connection

--- db/db.php
+++ db/db.php
@@ -4,10 +4,10 @@ $port = 23488;
 $db   = 'defaultdb';
 $user = 'avnadmin';
 $pass = getenv('DB_PASSWORD'); // "CLICK_TO_REVEAL_PASSWORD" ustiga bosib olasiz
-
+$ssl_cert = __DIR__ . '/ca.pem'; 
 // Aiven xavfsizlik uchun SSL talab qiladi (SSL mode: REQUIRED)
 $options = [
-    PDO::MYSQL_ATTR_SSL_CA => false, 
+    PDO::MYSQL_ATTR_SSL_CA => $ssl_cert, 
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
