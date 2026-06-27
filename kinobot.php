81e1f0a314756ec7494c154eba2ab54cd8f3a560
PyDev <pydev4150@gmail.com>
2026-06-27 21:23:58 +0500

    Update kinobot.php

--- kinobot.php
+++ kinobot.php
@@ -7,18 +7,7 @@ define('ADMIN_ID', getenv('SIZNING_ID_RAQAMINGIZ'));
 define('BASE_CHANNEL_ID', '-1004425933558'); // Maxfiy kanal ID si
 error_reporting(0);
 // MySQL bazaga ulanish ma'lumotlari
-$db_host = getenv('myhost');
-$db_user = 'kino63773537bot';
-$db_pass = getenv('BAZA_Paroli');
-$db_name = 'kino63773537bot_kino';
-
-try {
-    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-} catch (PDOException $e) {
-    die("Baza xatosi: " . $e->getMessage());
-}
+require './db/db.php';
 
 // Telegram API so'rov funksiyasi
 function bot($method, $datas = []) {
