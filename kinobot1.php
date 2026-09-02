49e42bef7095e479ba6525f17925d790f5bc1360
PyDev <pydev4150@gmail.com>
2026-09-02 19:09:49 +0500

    Update admin keyboard button URL for Kino upload

--- kinobot1.php
+++ kinobot1.php
@@ -231,7 +231,7 @@ if (isset($update->message)) {
         $admin_keyboard = json_encode([
             'resize_keyboard' => true,
             'keyboard' => [
-                [['text' => "🎬 Kino yuklash / Ko'chirish", 'web_app' => ['url' => 'https://www.google.com']], ['text' => "📊 Statistika"]],
+                [['text' => "🎬 Kino yuklash / Ko'chirish", 'web_app' => ['url' => './admin_app.php']], ['text' => "📊 Statistika"]],
                 [['text' => "📢 Kanallar"], ['text' => "⚙️ Sozlamalar"]],
                 [['text' => "📝 Start xabarini sozlash"]]
             ]
